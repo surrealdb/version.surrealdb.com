c11d63ab01d4400d1cedfdce2d4bf8dfcdbdc8e7
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-02-02 11:06:38 +0000

    Use Header Policies instead of Cloudfront Functions

--- aws/viewer-response/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-aws --profile surreal cloudfront describe-function --name version-surrealdb-com-viewer-response > temp.json
-ETAG=$(jq -r '.ETag' temp.json)
-aws --profile surreal cloudfront update-function --name version-surrealdb-com-viewer-response --function-code fileb://index.js --function-config Comment="",Runtime="cloudfront-js-1.0" --if-match $ETAG > temp.json
-ETAG=$(jq -r '.ETag' temp.json)
-aws --profile surreal cloudfront publish-function --name version-surrealdb-com-viewer-response --if-match $ETAG > temp.json
-rm -rf temp.json
