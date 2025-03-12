57ab8f2ff95ee48e2b172015c13ff49f65ba2aeb
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-03-12 21:12:27 +0000

    Update AWS Lambda deployment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ rm -rf code.zip
 zip -r --symlinks code.zip . -x '*.git*'
 aws s3 cp --region us-east-1 code.zip s3://deploy.surrealdb.com/version-surrealdb-com.zip
 aws lambda --region us-east-1 update-function-code --function-name version-surrealdb-com --s3-bucket deploy.surrealdb.com --s3-key version-surrealdb-com.zip && sleep 3
-aws lambda --region us-east-1 update-function-configuration --function-name version-surrealdb-com --runtime nodejs20.x --handler index.main --timeout 5 --memory-size 128 && sleep 3
+aws lambda --region us-east-1 update-function-configuration --function-name version-surrealdb-com --runtime nodejs22.x --handler index.main --timeout 5 --memory-size 512 && sleep 3
 ARN=$(aws lambda --region us-east-1 publish-version --function-name version-surrealdb-com | jq -r ".FunctionArn")
 rm -rf code.zip
 
